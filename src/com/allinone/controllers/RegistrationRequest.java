71a35f53b3bb5fa40a4a4ae77dc2d120984d95ea
pswargam <31352075+pswargam@users.noreply.github.com>
2017-09-28 20:48:13 -0400

    Registration

--- src/com/allinone/controllers/RegistrationRequest.java
+++ src/com/allinone/controllers/RegistrationRequest.java
@@ -1,5 +1,4 @@
 package com.allinone.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class LoginService {
-@RequestMapping(value="/login",method=RequestMethod.POST)
-@ResponseBody
-public String loginrequest( @RequestParam String name, @RequestParam String password) {
-	return "Welcome";
-}
+public class RegistrationRequest {
+	@RequestMapping(value="/register",method=RequestMethod.POST)
+	@ResponseBody
+	public String registerrequest( @RequestParam String name, @RequestParam String password,@RequestParam String email) {
+		return "Registration successful";
+	}
 }
