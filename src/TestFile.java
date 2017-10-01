86ebebbd253de83e3f4769d15f0fe7d96293cafa
rahpsing <rahpsing@iu.edu>
2017-10-01 02:18:43 -0400

    Updated identifier generation strategy and added a new user add test case

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,9 +1,46 @@
+import org.hibernate.Session;
+import org.junit.Test;
+
+import com.allinone.pojos.User;
+import com.allinone.util.HibernateUtil;
 
 public class TestFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		
+		User user = new User();
+
+		user.setAdmin(false);
+		user.setUserName("Rahul");
+		user.setEmailAddress("rahul_singh919@yahoo.com");
+		user.setPhoneNumber("8123698563");
+
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
 	}
 
+	@Test
+	public void testMethod() {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		
+		User user = new User();
+
+		user.setAdmin(false);
+		user.setUserName("Rahul");
+		user.setEmailAddress("rahul_singh919@yahoo.com");
+		user.setPhoneNumber("8123698563");
+
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+	}
 }
